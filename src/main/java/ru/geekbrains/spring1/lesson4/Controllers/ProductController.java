73b940c6b42bfd0532db53fa32f3dc83ab522591
unknown <zlovred12@gmail.com>
2022-08-19 19:28:03 +0300

    temp

--- src/main/java/ru/geekbrains/spring1/lesson4/Controllers/ProductController.java
+++ src/main/java/ru/geekbrains/spring1/lesson4/Controllers/ProductController.java
@@ -2,7 +2,12 @@ package ru.geekbrains.spring1.lesson4.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.geekbrains.spring1.lesson4.Entities.Product;
 import ru.geekbrains.spring1.lesson4.Services.ProductService;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -12,6 +17,13 @@ public class ProductController {
     @Autowired
     public void setProductService(ProductService productService) {
         this.productService = productService;
+    }
+
+
+    @GetMapping("/products")
+    @ResponseBody
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
     }
 
 }
